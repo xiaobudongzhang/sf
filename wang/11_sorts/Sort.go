1da4531580e3fea50b6a77061d294473cb1d30cf
xiaobudongzhang <xiaobudongzhang@fangdd.com>
2018-12-07 23:30:04 +0800

    queue linked

--- wang/11_sorts/Sort.go
+++ wang/11_sorts/Sort.go
@@ -1,14 +1,11 @@
 package _11_sorts
 
-import "fmt"
-
 func BubbleSort(a []int, n int)  {
 	for i := 0; i < n; i++  {
-		fmt.Printf("\nsort time:%v\n", i + 1)
 		change := false
-		for i := 0; i < n -1; i++  {
-			if a[i] > a[i+1] {
-				a[i+1], a[i] = a[i], a[i+1]
+		for j := 0; j < n - i -1; j++  {
+			if a[j] > a[j+1] {
+				a[j+1], a[j] = a[j], a[j+1]
 				change = true
 			}
 		}
