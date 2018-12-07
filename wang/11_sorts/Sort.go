732c9fa44faa64572c955603792cf39a03c90b4f
xiaobudongzhang <xiaobudongzhang@fangdd.com>
2018-12-07 23:26:10 +0800

    bubblesort

--- wang/11_sorts/Sort.go
+++ wang/11_sorts/Sort.go
@@ -1,1 +1,20 @@
-package _1_sorts
+package _11_sorts
+
+import "fmt"
+
+func BubbleSort(a []int, n int)  {
+	for i := 0; i < n; i++  {
+		fmt.Printf("\nsort time:%v\n", i + 1)
+		change := false
+		for i := 0; i < n -1; i++  {
+			if a[i] > a[i+1] {
+				a[i+1], a[i] = a[i], a[i+1]
+				change = true
+			}
+		}
+		if !change {
+			break
+		}
+	}
+
+}
---
+++ wang/11_sorts/Sort_test.go
@@ -0,0 +1,23 @@
+package _11_sorts
+
+import (
+	"testing"
+	"fmt"
+	"runtime"
+)
+
+func TestBubbleSort(t *testing.T) {
+	PrintFunc()
+	arr := []int{3, 5, 4, 1, 2, 6}
+	fmt.Printf("before sort:\n", arr)
+	BubbleSort(arr, len(arr))
+	fmt.Printf("after sort:\n", arr)
+}
+
+func PrintFunc()  {
+	fmt.Println("--------------------------------------")
+	funName, _,_,ok := runtime.Caller(1)
+	if ok {
+		fmt.Println("funName+" + runtime.FuncForPC(funName).Name())
+	}
+}
