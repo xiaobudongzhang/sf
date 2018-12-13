2e7681f6a8f876d0cd99abf8c1f71bae8ea4009c
xiaobudongzhang <xiaobudongzhang@fangdd.com>
2018-12-13 21:08:11 +0800

    a

--- wang/15_binarysearch/BinarySearchChange.go
+++ wang/15_binarysearch/BinarySearchChange.go
@@ -23,14 +23,64 @@ func FirstEqual(a []int) int {
 	return -1
 }
 
-func LastQqual()  {
-	
+func LastQqual(a []int) int {
+	lo := 0
+	hi := len(a) -1
+	s := 8
+	for lo <= hi{
+		mid := lo + ((hi - lo) / 2)
+		if a[mid] == s {
+			if ((mid == len(a) -1) || a[mid + 1] != s){//下一个值不等于说明现在的是最后一个
+				return mid
+			}else{
+				lo = mid + 1
+			}
+		}else if(a[mid] < s){
+			lo = mid + 1
+		}else {
+			hi = mid - 1
+		}
+	}
+
+	return -1
 }
 
-func FirstGtOrEqual()  {
-	
+func FirstGtOrEqual(a []int) int{
+	lo := 0
+	hi := len(a) -1
+	s := 9
+	for lo <= hi{
+		mid := lo + ((hi - lo) / 2)
+		if a[mid] >= s {
+			if ((mid == 0) || a[mid -1] < s){//前一个值小于说明现在的是第一个大于等于
+				return mid
+			}else{
+				hi = mid -1
+			}
+		}else{
+			lo = mid + 1
+		}
+	}
+
+	return -1
 }
 
-func LastLessOrEqual()  {
-	
+func LastLessOrEqual(a []int) int {
+	lo := 0
+	hi := len(a) -1
+	s := 11
+	for lo <= hi{
+		mid := lo + ((hi - lo) / 2)
+		if a[mid] <= s {
+			if ((mid == len(a) - 1) || a[mid + 1] > s){//前一个值小于说明现在的是第一个大于等于
+				return mid
+			}else{
+				lo = mid + 1
+			}
+		}else{
+			hi = mid -1
+		}
+	}
+
+	return -1
 }
--- wang/15_binarysearch/BinarySearchChange_test.go
+++ wang/15_binarysearch/BinarySearchChange_test.go
@@ -11,3 +11,24 @@ func TestFirstEqual(t *testing.T) {
 	index := FirstEqual(a)
 	fmt.Printf("index:%v\n", index)
 }
+
+func TestLastEqual(t *testing.T) {
+	PrintFunc()
+	a := []int{1,7,8,8,8,8,9,11}
+	index := LastQqual(a)
+	fmt.Printf("index:%v\n", index)
+}
+
+func TestFirstGtOrEqual(t *testing.T) {
+	PrintFunc()
+	a := []int{1,7,8,8,8,8,9,11}
+	index := FirstGtOrEqual(a)
+	fmt.Printf("index:%v\n", index)
+}
+
+func TestLastLessOrEqual(t *testing.T) {
+	PrintFunc()
+	a := []int{1,7,8,8,8,8,9,11}
+	index := LastLessOrEqual(a)
+	fmt.Printf("index:%v\n", index)
+}
