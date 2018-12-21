b1c68177b9dbb510c1709567cac2ca540894d0b0
xiaobudongzhang <xiaobudongzhang@fangdd.com>
2018-12-21 23:51:32 +0800

    src

---
+++ wang/src/23_tree/TreeTool.go
@@ -0,0 +1,15 @@
+package _3_tree
+
+import (
+	"08_stack"
+)
+
+//计算树的高度....
+
+
+//按层遍历
+
+func Te()  {
+	_8_stack.NewArrayStack(1)
+}
+
---
+++ wang/src/23_tree/TreeTool_test.go
@@ -0,0 +1,11 @@
+package _3_tree
+
+import (
+	"testing"
+	"fmt"
+)
+
+func TestTe(t *testing.T) {
+	Te()
+	fmt.Printf("xxxxxxxxxxxxxxxxxxxxxxxxxxxx")
+}
