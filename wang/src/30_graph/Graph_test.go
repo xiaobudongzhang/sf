461a1eaa08f81cf044b4cfd74e55ef494cf02c22
xiaobudongzhang <xiaobudongzhang@fangdd.com>
2018-12-24 23:51:25 +0800

    graph

---
+++ wang/src/30_graph/Graph_test.go
@@ -0,0 +1,23 @@
+package _0_graph
+
+import (
+	"testing"
+	"lib"
+	//"fmt"
+)
+
+func TestGraph_InsertA(t *testing.T) {
+	lib.PrintFunc()
+	graph := NewGraph(10)
+
+	graph.InsertA(1)
+	graph.InsertA(2)
+	graph.InsertA(3)
+
+	graph.InsertOfA(1,2)
+	graph.InsertOfA(2,3)
+	graph.InsertOfA(2,4)
+	graph.InsertOfA(2,5)
+
+	graph.Print()
+}
