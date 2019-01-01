a3396a6ae6d948769fbea32c2619fba995bcbffb
xiaobudongzhang <xiaobudongzhang@fangdd.com>
2019-01-01 15:35:54 +0800

    patter

--- wang/src/27_tree_rever/Rever.go
+++ wang/src/27_tree_rever/Rever.go
@@ -9,9 +9,9 @@ func Rever(arr []int, k int)  {
 		}
 		fmt.Println()
 	}
-
+	//先排列最后一位
 	for i := 0; i < k; i++ {
-		arr[i], arr[k-1] = arr[k-1], arr[i]
+		arr[i], arr[k-1] = arr[k-1], arr[i]//待排列的一位的所有可能性
 
 		Rever(arr, k -1)
 		arr[i], arr[k-1] = arr[k-1], arr[i]
