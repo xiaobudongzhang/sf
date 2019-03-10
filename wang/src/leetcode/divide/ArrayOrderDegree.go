4c003080b20de49a893b2b18631557e65187e5a2
xiaobudongzhang <xiaobudongzhang@fangdd.com>
2019-03-10 09:49:32 +0800

    add divide

---
+++ wang/src/leetcode/divide/ArrayOrderDegree.go
@@ -0,0 +1,5 @@
+package divide
+
+func ArrayOrderDegree(arr []int) int{
+	return 0
+}
---
+++ wang/src/leetcode/divide/ArrayOrderDegree_test.go
@@ -0,0 +1,10 @@
+package divide
+
+import (
+	"testing"
+	"lib"
+)
+
+func TestArrayOrderDegree(t *testing.T) {
+	lib.PrintFunc()
+}
