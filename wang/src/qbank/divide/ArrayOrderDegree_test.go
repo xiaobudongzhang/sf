9ae1b512381719e78d353ca88ede40de8b832b52
xiaobudongzhang <xiaobudongzhang@fangdd.com>
2019-03-10 11:34:30 +0800

    求数组的逆序度

--- wang/src/qbank/divide/ArrayOrderDegree_test.go
+++ wang/src/qbank/divide/ArrayOrderDegree_test.go
@@ -3,8 +3,13 @@ package divide
 import (
 	"testing"
 	"lib"
+	"fmt"
 )
 
 func TestArrayOrderDegree(t *testing.T) {
 	lib.PrintFunc()
+	arr := []int{1,5,6,2,4,3}
+	count := ArrayOrderDegree(arr)
+	fmt.Printf("%v\n", count)
+	fmt.Printf("%v\n", arr)
 }
