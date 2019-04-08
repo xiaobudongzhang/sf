1dc78d21e746d076499a1db83e172ffafb2f9013
byz <xiaobudongzhang@fangdd.com>
2019-04-09 07:52:50 +0800

    定义跳表基础架构

--- data_structure/skip_list/SkipList.go
+++ data_structure/skip_list/SkipList.go
@@ -1,2 +1,31 @@
 package skip_list
 
+type SkipListNode struct {
+	ele interface{}
+	score float32
+	level[] SkipListLevel
+}
+
+type SkipListLevel struct {
+	forward *SkipListNode
+	span int //跨度，快速计算出排名
+}
+
+type SkipList struct {
+		head *SkipListNode
+		tail *SkipListNode
+		level int
+}
+
+func (this *SkipListNode) Insert(ele interface{}, score float32) bool {
+	return true
+}
+
+func (this *SkipListNode) Get(ele interface{})  interface{}{
+	return nil
+}
+
+func (this *SkipListNode) GetRank(ele interface{})  int {
+	return 0
+}
+
