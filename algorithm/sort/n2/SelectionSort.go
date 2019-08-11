025e286191b85f31a5410d8cf810c8fe58b7ec8a
xiaobudongzhang <xiaobudongzhang@fangdd.com>
2019-08-11 16:04:12 +0800

    排序(选择)

--- algorithm/sort/n2/SelectionSort.go
+++ algorithm/sort/n2/SelectionSort.go
@@ -1,1 +1,15 @@
 package n2
+
+func SelectionSort(a []int, n int) []int {
+	if n <= 1 {
+		return a
+	}
+	for i := 0; i < n -1; i++ {
+		for j := i + 1; j < n; j++ {
+			if a[j] < a[i] {
+				a[j], a[i] = a[i], a[j]
+			}
+		}
+	}
+	return a
+}
--- algorithm/sort/n2/SelectionSort_test.go
+++ algorithm/sort/n2/SelectionSort_test.go
@@ -1,1 +1,15 @@
 package n2
+
+import (
+	"fmt"
+	"github.com/xiaobudongzhang/sf/lib"
+	"testing"
+)
+
+func TestSelectionSort(t *testing.T) {
+	lib.PrintFunc()
+	arr := []int{4, 5, 6, 1, 3, 2}
+	fmt.Printf("before sort:%v\n", arr)
+	SelectionSort(arr, len(arr))
+	fmt.Printf("after sort:%v\n", arr)
+}
