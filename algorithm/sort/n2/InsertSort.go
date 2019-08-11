df8174d5005990b111cf4519eae1c0e7c8766edb
xiaobudongzhang <xiaobudongzhang@fangdd.com>
2019-08-11 15:45:47 +0800

    排序(插入)

--- algorithm/sort/n2/InsertSort.go
+++ algorithm/sort/n2/InsertSort.go
@@ -1,1 +1,21 @@
 package n2
+
+func InsertSort(a []int, n int) []int  {
+	if n <= 1 {
+		return a
+	}
+	for i := 1; i < n; i++ {//不断将i插入已排序的字段
+		for j := i; j > 0;j--  {
+
+			if a[j] >= a[j-1] {
+				break
+			}
+
+			if a[j] < a[j-1] {
+				a[j], a[j-1] = a[j-1],a[j]
+			}
+
+		}
+	}
+	return a
+}
--- algorithm/sort/n2/InsertSort_test.go
+++ algorithm/sort/n2/InsertSort_test.go
@@ -1,1 +1,15 @@
 package n2
+
+import (
+	"fmt"
+	"github.com/xiaobudongzhang/sf/lib"
+	"testing"
+)
+
+func TestInsertSort(t *testing.T) {
+	lib.PrintFunc()
+	arr := []int{4, 5, 6, 1, 3, 2}
+	fmt.Printf("before sort:%v\n", arr)
+	InsertSort(arr, len(arr))
+	fmt.Printf("after sort:%v\n", arr)
+}
