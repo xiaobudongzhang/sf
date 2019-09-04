2edf9ccd38b28007d0e6c2026aa2c87743e385f7
xiaobudongzhang <xiaobudongzhang@fangdd.com>
2019-09-05 07:53:41 +0800

    添加邻接矩阵

--- data_structure/graph/GraphMatrix.go
+++ data_structure/graph/GraphMatrix.go
@@ -1,3 +1,25 @@
 package graph
 //邻接矩阵
+type GraphMatrix struct {
+	datas [][]int
+	size int
+}
 
+func NewGraphMatrix(size int) *GraphMatrix{
+	return &GraphMatrix{size:size}
+}
+
+func (graph *GraphMatrix) AddEdge(a int, b int, weight int) bool {
+	if a > graph.size || b > graph.size {
+		return false
+	}
+	
+	return true
+}
+
+func (graph *GraphMatrix) RemoveEdge(a int, b int) bool  {
+	if a > graph.size || b > graph.size {
+		return false
+	}
+	return true
+}
