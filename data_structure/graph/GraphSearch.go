77822d53ddf96d7d597eb87b0a8c60bfdcd3d728
xiaobudongzhang <xiaobudongzhang@fangdd.com>
2019-09-07 11:12:17 +0800

    add GraphSearch

--- data_structure/graph/GraphSearch.go
+++ data_structure/graph/GraphSearch.go
@@ -1,1 +1,55 @@
 package graph
+
+import (
+	"container/list"
+	"fmt"
+)
+
+//无向图为基础
+//todo 去重
+
+type GraphSearch struct {
+	size int
+	adj []*list.List
+}
+
+func newGraphSearch(size int) *GraphSearch{
+	adj := make([]*list.List, size+1)
+	for k,_ :=range adj {
+		adj[k] = &list.List{}
+	}
+	return &GraphSearch {
+		size:size+1,
+		adj:adj,
+	}
+}
+func (graph *GraphSearch) AddEdge(s int, t int) bool {
+	if s > graph.size || s < 0 || t > graph.size || t < 0 {
+		return false
+	}
+	graph.adj[s].PushBack(t)
+	graph.adj[t].PushBack(s)
+	return true
+}
+//广度搜索
+func (graph *GraphSearch) Bfs(s int, t int) bool {
+	return true
+}
+//深度搜索
+func (graph *GraphSearch) Dfs(s int, t int) bool {
+	return true
+}
+
+func (graph *GraphSearch) Print() {
+	for aEdge,ls :=range graph.adj {
+		fmt.Printf("%v ==> ", aEdge)
+
+		current := ls.Front()
+		for current != nil {
+			fmt.Printf(" %v ", current.Value.(int))
+			current = current.Next()
+		}
+
+		fmt.Println()
+	}
+}
--- data_structure/graph/GraphSearch_test.go
+++ data_structure/graph/GraphSearch_test.go
@@ -1,1 +1,23 @@
 package graph
+
+import (
+	"github.com/xiaobudongzhang/sf/lib"
+	"testing"
+)
+
+func TestGraphSearch_Bfs(t *testing.T) {
+	lib.PrintFunc()
+	graph := newGraphSearch(10)
+	graph.AddEdge(0, 1)
+	graph.AddEdge(0, 3)
+	graph.AddEdge(1, 2)
+	graph.AddEdge(1, 4)
+	graph.AddEdge(2, 5)
+	graph.AddEdge(3, 4)
+	graph.AddEdge(4, 5)
+	graph.AddEdge(4, 6)
+	graph.AddEdge(5, 7)
+	graph.AddEdge(6, 7)
+
+	graph.Print()
+}
