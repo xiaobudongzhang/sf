505df760412f29f92e0209b77711f7339bbffbc4
xiaobudongzhang <xiaobudongzhang@fangdd.com>
2019-09-07 09:30:30 +0800

    add method

--- data_structure/graph/GraphMatrix.go
+++ data_structure/graph/GraphMatrix.go
@@ -6,20 +6,35 @@ type GraphMatrix struct {
 }
 
 func NewGraphMatrix(size int) *GraphMatrix{
-	return &GraphMatrix{size:size}
+
+	datas := make([][]int, size+1)
+	for k, _ :=range datas  {
+		subdata := make([]int, size + 1)
+		datas[k] = subdata
+	}
+	return &GraphMatrix{
+		datas:datas,
+		size:size+1,
+	}
 }
 
 func (graph *GraphMatrix) AddEdge(a int, b int, weight int) bool {
-	if a > graph.size || b > graph.size {
+	if a > graph.size || b > graph.size || a < 1 || b < 1 || weight < 1{
 		return false
 	}
-
+	graph.datas[a][b] = weight
 	return true
 }
 
 func (graph *GraphMatrix) RemoveEdge(a int, b int) bool  {
-	if a > graph.size || b > graph.size {
+	if a > graph.size || b > graph.size || a < 1 || b < 1 {
 		return false
 	}
+	graph.datas[a][b] = 0
 	return true
+}
+
+
+func (graph *GraphMatrix) Data() [][]int  {
+	return graph.datas
 }
--- data_structure/graph/GraphMatrix_test.go
+++ data_structure/graph/GraphMatrix_test.go
@@ -1,1 +1,22 @@
 package graph
+
+import (
+	"fmt"
+	"github.com/xiaobudongzhang/sf/lib"
+	"testing"
+)
+
+func TestGraphMatrix_AddEdge(t *testing.T) {
+	lib.PrintFunc()
+	graph := NewGraphMatrix(10)
+	graph.AddEdge(1,1 ,2)
+	fmt.Printf("graph:%v\n", graph.Data())
+}
+
+func TestGraphMatrix_RemoveEdge(t *testing.T) {
+	lib.PrintFunc()
+	graph := NewGraphMatrix(10)
+	graph.AddEdge(1,1 ,2)
+	graph.RemoveEdge(1,1)
+	fmt.Printf("graph:%v\n", graph.Data())
+}
